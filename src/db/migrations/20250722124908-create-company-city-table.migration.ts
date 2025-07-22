73f2f3fc743c40acf6d765e6794d4e05ff50fc83
Pratham <sinhapratham1234@gmail.com>
2025-07-22 18:27:28 +0530

    #feat: added company-city-table

---
+++ src/db/migrations/20250722124908-create-company-city-table.migration.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+import { QueryInterface } from 'sequelize';
+
+
+export default {
+    async up (queryInterface: QueryInterface) {
+        await queryInterface.sequelize.query(`
+        CREATE TABLE company_cities (
+          company_id INT NOT NULL,
+          city_id INT NOT NULL,
+          created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+          updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+          PRIMARY KEY (company_id, city_id),
+        );
+      `);
+
+    },
+
+    async down (queryInterface: QueryInterface) {
+        await queryInterface.sequelize.query(`
+          DROP TABLE IF EXISTS company_cities;
+        `);
+    }
+};
