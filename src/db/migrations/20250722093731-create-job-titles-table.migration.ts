f92625aa90df9c93c329a1d9256dd83be73da985
Pratham <sinhapratham1234@gmail.com>
2025-07-22 16:04:22 +0530

    #fix: fixed typoes in migration

--- src/db/migrations/20250722093731-create-job-titles-table.migration.ts
+++ src/db/migrations/20250722093731-create-job-titles-table.migration.ts
@@ -9,7 +9,7 @@ module.exports = {
             `
             CREATE TABLE job_titles (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
+                title VARCHAR(255) NOT NULL UNIQUE,
                 created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
                 updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 deleted_at DATETIME DEFAULT NULL
