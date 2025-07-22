b6271b07859c9ad89b1e9e3f2a4f092b990ba577
Pratham <sinhapratham1234@gmail.com>
2025-07-22 16:25:47 +0530

    #feat: added employmentType model and its migration

---
+++ src/db/migrations/20250722104827-create-employment-type-table.migration.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+import { QueryInterface } from 'sequelize';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up (queryInterface: QueryInterface) {
+        await queryInterface.sequelize.query(`
+        CREATE TABLE employment_types (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          name VARCHAR(20) NOT NULL UNIQUE,
+          description VARCHAR(255) ,
+          created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+          updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+          deleted_at DATETIME DEFAULT NULL
+        );
+    `);
+    },
+
+    async down (queryInterface: QueryInterface) {
+        await queryInterface.sequelize.query(`
+          DROP TABLE IF EXISTS employment_types;
+        `);
+
+    }
+};
